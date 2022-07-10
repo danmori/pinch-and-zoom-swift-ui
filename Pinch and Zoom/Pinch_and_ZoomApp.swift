114ddec62bfebbede68c8f1e6e6e8828119307ba
Dan Mori <danielstelzer@gmail.com>
2022-07-10 20:21:38 -0300

    Initial Commit

---
+++ Pinch and Zoom/Pinch_and_ZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_and_ZoomApp.swift
+//  Pinch and Zoom
+//
+//  Created by Dan Mori on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_and_ZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
